8d48a0565dc03769d6d3a39f11d05464411a20a8
Princy-Murugaraj <princymurugaraj@gmail.com>
2025-05-28 19:57:18 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class NumberGame {
+    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int chances = 8;
+            int finals = 0;
+            boolean playAgain = true;
+            System.out.println("Welcome buddy,!");
+            System.out.println("Hey Buddy, you have about " + chances + " chances to win the game");
+
+            while(playAgain){
+                int rand = getrandN(1, 100);
+                boolean guess = false;
+
+                for(int i = 0; i < chances; i++){
+                    System.out.println("chance " + (i+1) + ": Enter your guess:");
+                    sc.nextLine();
+                    int user = sc.nextInt();
+
+                    if (user == rand){
+                        guess = true;
+                        finals += 1;
+                        System.out.println("You won it");
+                        break;
+                    } else if (user > rand) {
+                        System.out.println("Too High.");
+                    } else {
+                        System.out.println("Too Low.");
+
+                    }
+                    }
+                    if (!guess){
+                        System.out.println("Sorry Buddy, you lost the chances. The number was " + rand);
+                    }
+                    System.out.println("Do you want to play again (Yes/No)");
+                    String pA = sc.next();
+                    playAgain = pA.equalsIgnoreCase("Yes");
+
+}
+System.out.println( "That's it buddy, Hope you enjoyed it");
+System.out.println("Here is your score: " + finals);
+        }
+            }
+
+    public static int getrandN(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+        }
+    
+
